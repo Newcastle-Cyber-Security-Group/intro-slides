34ae6226293326fc9154315aa9c183b4d8df4967
jayrovacsek <jay@rovacsek.com>
2024-08-20 20:40:36 +1000

    feat: utilise numtide's devshell refactor: reformat nix files with rfc package refactor: remove shell.nix chore: bump inputs to recent updates of nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,83 +2,89 @@
   description = "NCSG Introduction Slides";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    devshell = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/devshell";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
 
     nixpkgs.url = "github:nixOS/nixpkgs/nixpkgs-unstable";
 
-    pre-commit-hooks = {
+    git-hooks = {
+      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:cachix/pre-commit-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-        flake-compat.follows = "flake-compat";
-      };
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      devshell,
+      flake-utils,
+      nixpkgs,
+      self,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays.git-cliff = _final: prev: {
-          git-cliff = prev.rustPlatform.buildRustPackage rec {
-            inherit (prev.git-cliff) doCheck meta pname;
-
-            version = "2.0.2";
-
-            buildInputs = prev.git-cliff.buildInputs
-              ++ (prev.lib.optionals prev.stdenv.isDarwin
-                (with prev.darwin.apple_sdk.frameworks; [
-                  CoreServices
-                  SystemConfiguration
-                ]));
-
-            src = prev.fetchFromGitHub {
-              owner = "orhun";
-              repo = "git-cliff";
-              rev = "v${version}";
-              hash = "sha256-m8xnsj6z/QBeya3CQBkQ+/eGSCZVKpTa8y1zt+3NeIo=";
-            };
-
-            cargoHash = "sha256-axh62ogKI2UnlI4aXLDB3fIg1CowQN8xRKWmZi5Kgig=";
-          };
-        };
-
         pkgs = import nixpkgs {
+          overlays = [ devshell.overlays.default ];
           inherit system;
-          overlays = [ overlays.git-cliff ];
         };
-      in {
+      in
+      {
         apps = {
           build = {
             type = "app";
-            program = builtins.toString (pkgs.writers.writeBash "build" ''
-              ${pkgs.hugo}/bin/hugo -s .
-            '');
+            program = builtins.toString (
+              pkgs.writers.writeBash "build" ''
+                ${pkgs.hugo}/bin/hugo -s .
+              ''
+            );
           };
 
           develop = {
             type = "app";
-            program = builtins.toString (pkgs.writers.writeBash "develop" ''
-              ${pkgs.hugo}/bin/hugo server
-            '');
+            program = builtins.toString (
+              pkgs.writers.writeBash "develop" ''
+                ${pkgs.hugo}/bin/hugo server
+              ''
+            );
           };
         };
 
-        checks.pre-commit = self.inputs.pre-commit-hooks.lib.${system}.run {
+        checks.pre-commit = self.inputs.git-hooks.lib.${system}.run {
           src = self;
           hooks = {
             # Builtin hooks
             actionlint.enable = true;
             conform.enable = true;
-            deadnix.enable = true;
-            nixfmt.enable = true;
-            prettier.enable = true;
+            deadnix = {
+              enable = true;
+              settings.edit = true;
+            };
+
+            nixfmt = {
+              enable = true;
+              package = pkgs.nixfmt-rfc-style;
+              settings.width = 80;
+            };
+
+            prettier = {
+              enable = true;
+              settings = {
+                ignore-path = [ self.packages.${system}.prettierignore ];
+                write = true;
+              };
+            };
+
             statix.enable = true;
-            typos.enable = true;
+
+            typos = {
+              enable = true;
+              settings.locale = "en-au";
+            };
 
             # Custom hooks
             git-cliff = {
@@ -88,6 +94,7 @@
               language = "system";
               pass_filenames = false;
             };
+
             statix-write = {
               enable = true;
               name = "Statix Write";
@@ -96,50 +103,35 @@
               pass_filenames = false;
             };
           };
-
-          # Settings for builtin hooks, see also: https://github.com/cachix/pre-commit-hooks.nix/blob/master/modules/hooks.nix
-          settings = {
-            deadnix.edit = true;
-            nixfmt.width = 80;
-            prettier = {
-              ignore-path = [ self.packages.${system}.prettierignore ];
-              write = true;
-            };
-            typos.locale = "en-au";
-          };
         };
 
-        devShells = let
+        devShells.default = pkgs.devshell.mkShell {
+          devshell.startup.git-hooks.text = self.checks.${system}.pre-commit.shellHook;
           name = "hugo-revealjs";
-          nodePackages = with pkgs.nodePackages; [ eslint prettier ];
-          packages =
-            (with pkgs; [ deadnix go hugo nixfmt statix typos trivy vulnix ])
-            ++ nodePackages;
-        in {
-          default = self.devShells.${system}.${name};
-          "${name}" = pkgs.mkShell {
-            inherit name packages;
-            inherit (self.checks.${system}.pre-commit) shellHook;
-          };
+          packages = with pkgs; [
+            deadnix
+            go
+            hugo
+            nixfmt-rfc-style
+            nodePackages.prettier
+            statix
+            typos
+            vulnix
+          ];
         };
 
-        packages = {
-          default = self.packages.${system}.prettierignore;
+        formatter = pkgs.nixfmt-rfc-style;
 
-          # Added as a simple convenience until https://github.com/NixOS/nixpkgs/pull/290950 is
-          # merged into the unstable branch
-          inherit (pkgs) git-cliff;
-
-          prettierignore = pkgs.writeTextFile {
-            name = ".prettierignore";
-            text = pkgs.lib.concatStringsSep "\n" [
-              ".pre-commit-config.yaml"
-              ".prettierignore"
-              "*.nix"
-              "CHANGELOG.md"
-              "result"
-            ];
-          };
+        packages.prettierignore = pkgs.writeTextFile {
+          name = ".prettierignore";
+          text = pkgs.lib.concatStringsSep "\n" [
+            ".pre-commit-config.yaml"
+            ".prettierignore"
+            "*.nix"
+            "CHANGELOG.md"
+            "result"
+          ];
         };
-      });
+      }
+    );
 }
